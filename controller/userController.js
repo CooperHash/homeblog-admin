eb54196b0b36bf160dfe9b9eacf338f4b8fdf94a
CooperHash <2597905281@qq.com>
2022-03-24 17:21:03 +0800

    ..

--- controller/userController.js
+++ controller/userController.js
@@ -9,7 +9,7 @@ saveUser = (req, res) => {
   const token = jwt.sign(
     { user: { name: params.username, password: params.userpassword } },
     SECRET_KEY,
-    { expiresIn: '29 days' }
+    { expiresIn: '30 days' }
   )
   console.log('🚀 → token', token)
   var callBack = (err) => {
