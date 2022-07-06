92dba1f1aa01100c38e2a5cc042b72a4493d3fb8
CooperHash <2597905281@qq.com>
2022-07-06 22:14:26 +0800

    fix

--- controller/videoController.js
+++ controller/videoController.js
@@ -16,6 +16,25 @@ getAllVideo = (req, res) => {
   dbConfig.sqlConnect(sql, sqlArr, callback);
 }
 
+
+getVideo = (req, res) => {
+  let {vload} = req.query
+  var sql = 'select title,content from Article where vload=?';
+  var sqlArr = [id];
+  var callback = (err, data) => {
+    if(err) {
+      console.log('连接出错');
+      console.log(err);
+    } else {
+      return res.send(
+        {'data': data}
+      );
+    }
+  }
+  dbConfig.sqlConnect(sql, sqlArr, callback);
+}
+
 module.exports = {
   getAllVideo,
+  getVideo
 }
--- routes/video/video.js
+++ routes/video/video.js
@@ -3,4 +3,5 @@ var router = express.Router();
 var video = require('../../controller/videoController')
 /* GET home page. */
 router.get('/api/getAllVideo',video.getAllVideo);
+router.get('/api/getVideo',video.getVideo);
 module.exports = router;
